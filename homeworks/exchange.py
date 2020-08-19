c0ba1cb6879144fcce38c6dee3b3ba6d1fcc0436
Robert Pietrzykowski <robert.pietrzykowski@pierce-ecom.com>
2020-08-19 20:32:17 +0200

    Add data to CSV.

--- homeworks/exchange.py
+++ homeworks/exchange.py
@@ -1,21 +1,21 @@
+import csv
 import functools
-
 import requests
-from datetime import datetime
 import time
+from datetime import datetime
 
 
 def get_datetime():
     actual_time = datetime.now()
     formatted_time = actual_time.strftime("%d.%m.%Y %H:%M:%S")
-    return f'Data i godzina: {formatted_time}'
+    return formatted_time
 
 
 def get_res_time(res):
     @functools.wraps(res)
     def wrapper(*args, **kwargs):
         milisec = int(res().elapsed.microseconds * 0.001)
-        print(f'Czas wykonania zapytania: {milisec}ms')
+        return milisec
     return wrapper
 
 
@@ -26,8 +26,11 @@ def get_res():
     params = {'base': base_currency}
     try:
         res = requests.get(url, params)
-        print(extract_eur_to_pln(res.json()))
-        print(get_datetime())
+        miliseconds = int(res.elapsed.microseconds * 0.001)
+        print(f'Kurs EUR {extract_eur_to_pln(res.json())}')
+        print(f'Data i godzina: {get_datetime()}')
+        print(f'Czas wykonania zapytania: {miliseconds} ms')
+        write_to_csv(extract_eur_to_pln(res.json()), get_datetime(), miliseconds)
         return res
     except TimeoutError:
         print("Response time from endpoint was too long.")
@@ -35,11 +38,18 @@ def get_res():
 
 def extract_eur_to_pln(response_json):
     currency_data = response_json["rates"]
-    return f'Kurs EUR {currency_data["PLN"]}'
+    return currency_data["PLN"]
+
+
+def write_to_csv(rate, date_time, request_time):
+    with open('exchange.csv', mode='a') as exchange_file:
+        fieldnames = ['Kurs EUR', 'Data i Godzina', 'Czas wykonania']
+        exchange_writer = csv.DictWriter(exchange_file, fieldnames=fieldnames)
+        exchange_writer.writerow({'Kurs EUR': rate, 'Data i Godzina':date_time, 'Czas wykonania': request_time})
 
 
 while True:
-    if datetime.now().second % 15 == 0:
+    # if datetime.now().second % 15 == 0:
         print('----------------------------------------------')
         get_res()
         print('----------------------------------------------')
